164a16210f851e1420e3ad065376c8dffb70ae4d
Hazzari <Freee2mmm@gmail.com>
2020-09-01 17:15:00 +0300

    add urls and view 'blog/', add models register Blog model to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+# Регистрация модели в админской панели
+# 2 варианта:
+# 1).
+# admin.site.register(Post)
+# 2).
+@admin.register(Post)
+class Admin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status',)
+    list_filter = ('status', 'created', 'publish', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
