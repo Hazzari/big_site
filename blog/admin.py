07bf7c3cf3712bec3b982e24cbdfbfd6326608a1
Hazzari <Freee2mmm@gmail.com>
2020-09-01 23:00:40 +0300

    add comment

--- blog/admin.py
+++ blog/admin.py
@@ -9,9 +9,15 @@ from .models import Post
 # 2).
 @admin.register(Post)
 class Admin(admin.ModelAdmin):
+    # Отображает заголовки колонок
     list_display = ('title', 'slug', 'author', 'publish', 'status',)
+    # Добавляет сбоку фильтр по категориям
     list_filter = ('status', 'created', 'publish', 'author')
+    # Заполнять slug автоматом из поля title
     prepopulated_fields = {'slug': ('title',)}
+    # выбрать автора по поиску
     raw_id_fields = ('author',)
+    # дата сверху мелкой строкой
     date_hierarchy = 'publish'
+    # умолчальная сортировка
     ordering = ('status', 'publish')
