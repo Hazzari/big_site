b2899da1719a4e15f40e8375a864e6aea4da7b1c
Hazzari <Freee2mmm@gmail.com>
2020-09-06 14:26:38 +0300

    add custome tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,39 @@
+from django import template
+from django.db.models import Count
+
+from ..models import Post
+
+# Регистрация своих тегов происходит через экземпляр Library
+register = template.Library()
+
+
+# Регистрируем тег
+@register.simple_tag
+def total_posts():
+    """
+    Возвращаем количество статей.
+
+    :return: QuerySet
+    """
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_post.html')
+def show_latest_posts(count=5):
+    """
+    последние статьи
+
+    :param count: количество статей.
+    :return: dict
+    """
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    # annotate() - добавляем к каждой статье количества комментариев.
+    # Count - используется в качестве функции агрегации,
+    # которая вычисляет количество комментариев total_comments
+
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
